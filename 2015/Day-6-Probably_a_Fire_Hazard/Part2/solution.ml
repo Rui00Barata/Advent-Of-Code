b70322dbb2a1b4fa24eec783c395b839bcdb11e9
Rui00Barata <rui.barata@ubi.pt>
2022-11-21 10:56:23 +0000

    Day 6 solutions

--- 2015/Day-6-Probably_a_Fire_Hazard/Part2/solution.ml
+++ 2015/Day-6-Probably_a_Fire_Hazard/Part2/solution.ml
@@ -25,23 +25,22 @@ let follow_instructions (t,(x1,y1),(x2,y2)) =
   let toggle (x1,y1) (x2,y2) = 
     for i=x1 to x2 do
       for j = y1 to y2 do
-        match lights.(i).(j) with
-        | 0 -> lights.(i).(j) <- 1
-        | _ -> lights.(i).(j) <- 0
+        lights.(i).(j) <- lights.(i).(j) + 2
       done
     done
   in
   let turn_on (x1,y1) (x2,y2) =
     for i=x1 to x2 do
       for j = y1 to y2 do
-       lights.(i).(j) <- 1
+        lights.(i).(j) <- lights.(i).(j) + 1
       done
     done
   in
   let turn_off (x1,y1) (x2,y2) =
     for i=x1 to x2 do
       for j = y1 to y2 do
-       lights.(i).(j) <- 0
+        if lights.(i).(j) > 0 then 
+          lights.(i).(j) <- lights.(i).(j) - 1
       done
     done
   in
@@ -51,7 +50,7 @@ let follow_instructions (t,(x1,y1),(x2,y2)) =
   | _ -> turn_off (x1,y1) (x2,y2)
 
 let count_turned_on () = 
-  Array.fold_left (fun acc al -> (Array.fold_left (fun acc1 x -> (if x=1 then 1 else 0) + acc1) 0 al) + acc) 0 lights
+  Array.fold_left (fun acc al -> (Array.fold_left (fun acc1 x -> x + acc1) 0 al) + acc) 0 lights
 
 
 let () = 
