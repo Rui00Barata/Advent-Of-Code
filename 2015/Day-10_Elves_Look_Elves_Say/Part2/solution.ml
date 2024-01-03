8f0e0ad1433d97f7aa724bd5079c4dc01e75e1ba
rui00barata <rui.barata@ubi.pt>
2024-01-03 01:24:43 +0000

    Updated File Structure and add Day 10 solutions

---
+++ 2015/Day-10_Elves_Look_Elves_Say/Part2/solution.ml
@@ -0,0 +1,28 @@
+let iter_string s =
+  let new_string = Buffer.create ((String.length s) * 2) in
+  let rec aux last_c count pos =
+    try
+      match s.[pos] with
+      | curr_c when last_c = curr_c -> aux last_c (count+1) (pos+1)
+      | curr_c                      -> 
+          Buffer.add_char new_string (char_of_int (count+48)); 
+          Buffer.add_char new_string last_c; 
+          aux curr_c 1 (pos+1)
+    with _ -> 
+      Buffer.add_char new_string (char_of_int (count+48)); 
+      Buffer.add_char new_string last_c; 
+      Buffer.contents new_string
+  in
+  aux s.[0] 1 1
+
+
+let apply s =
+  let rec aux idx s = 
+    if idx = 50 then s 
+    else aux (idx+1) (iter_string s)
+  in
+  aux 0 s
+
+
+let s = read_line ()
+let () = Printf.printf "%d\n" (String.length (apply s))
